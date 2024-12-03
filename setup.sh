43ba4d3963dde7b71b6ef5bc6825704990338fd9
MD <iammd5679@gmail.com>
2024-12-03 17:10:07 +0530

    ✨ Implement loading spinner for better user feedback

--- setup.sh
+++ setup.sh
@@ -5,3 +5,15 @@ show_header() {
     echo -e "\033[1;34m==============================================\033[0m"
     sleep 1
 }
+# Function to display a loading spinner
+show_spinner() {
+    spinner=( '⠋' '⠙' '⠹' '⠸' '⠼' '⠴' '⠦' '⠧' '⠇' '⠏' )
+    while [ $spin -eq 1 ]
+    do
+        for i in "${spinner[@]}"
+        do
+            echo -ne "\r\033[1;32m${i} $1 \033[0m"
+            sleep 0.1
+        done
+    done
+}
